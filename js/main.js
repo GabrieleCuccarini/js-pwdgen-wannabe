05f40695e735a8577115429489fa4f8e71327c05
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2022-10-26 16:23:45 +0200

    Completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 const userName = prompt ("Inserisci il tuo nome")
 const userSurname = prompt ("Inserisci il tuo cognome")
 const userColor = prompt ("Inserisci il tuo colore preferito")
+
+const userna = document.getElementById("usernamecon");
+// userna.innerHTML = userName
+
+const usersurna = document.getElementById("usersurnamecon");
+// usersurna.innerHTML = userSurname
+
+const usercolorh = document.getElementById("usercolorcon");
+// usercolorh.innerHTML = userColor
+
+console.log(userName);
+console.log(userSurname);
+console.log(userColor);
+
+const fraseDef = document.getElementById("frasecomplex");
+fraseDef.innerHTML = `${userName}${userSurname}${userColor}21`;
